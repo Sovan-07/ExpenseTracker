13e932151c7274e5ea6518d8b90e1421a0a1b2c7
Sovan Mondal <sovanmondal2003m@gmail.com>
2026-03-28 19:31:07 +0530

    navbar added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,36 @@
+import { Route, Routes, useNavigate } from "react-router-dom"
+import Layout from "./components/Layout"
+import Dashboard from "./pages/Dashboard"
+import { useState } from "react"
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [token , setToken] = useState(null);
+  const navigate = useNavigate();
+  const clearAuth = ()=>{
+    try {
+      localStorage.removeItem("user");
+      localStorage.removeItem("token");
+      sessionStorage.removeItem("user");
+      sessionStorage.removeItem("token");
+    }
+    catch(e) {
+      console.error('clearAuth Error: ' , e)
+    }
+    setUser(null);
+    setToken(null);
+  }
+  const onLogout = ()=>{
+    clearAuth();
+    navigate('/login');
+  }
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-         Hello world!
-      </h1>
+      <Routes>
+        <Route element={<Layout onLogout={onLogout} user = {user} />}>
+          <Route path="/" element={<Dashboard/>}/>
+        </Route>
+      </Routes>
     </>
   )
 }
